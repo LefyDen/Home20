e45a5ca01664efc210d9e913be9435c8903ec8e2
LefyDen <denisyuk.denis.s@gmail.com>
2022-10-02 00:48:44 +0300

    first commit

---
+++ Hopee.sql
@@ -0,0 +1,38 @@
+USE [TestDb]
+GO
+
+/****** Object:  Table [dbo].[Hope]    Script Date: 01.10.2022 21:10:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+GO
+
+
+
+
+SELECT * FROM Hope
+WHERE Age > 18
+
+SELECT * FROM Hope
+WHERE Adress IS NULL
+
+UPDATE Hope
+SET Age = Age + 1
+
+SELECT * FROM Hope
+
+ DELETE FROM  Hope
+   WHERE Adress Is NULL 
+SELECT * FROM Hope
+
+SELECT COUNT(*) FROM Hope
+
+
+
+SELECT Age, COUNT(*) AS SameAge
+FROM Hope
+GROUP BY Age
